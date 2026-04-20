0a4dcd15e62c1ab1d7dca62270549aca2a4833b2
Emily Fogaca <emilyrodrigues17@hotmail.com>
2026-04-20 14:54:27 -0400

    fix: criacao de filtro dinamico para a listagem de insumos

--- src/utils/createFilterInsumo.ts
+++ src/utils/createFilterInsumo.ts
@@ -5,22 +5,33 @@ export interface InsumoQueryParams {
     nome?: string;
     codigo?: string;
     categoriaId?: string;
+    statusEstoque?: string;
 }
 
-export function createFilter(dados: InsumoQueryParams): FindOptionsWhere<Insumo> {
-    const filtros: FindOptionsWhere<Insumo> = {};
+export function createFilter(dados: InsumoQueryParams): FindOptionsWhere<Insumo> |  FindOptionsWhere<Insumo>[] {
+
+    if (!dados.nome && !dados.codigo) {
+        const filtros: FindOptionsWhere<Insumo> = {};
+        if (dados.categoriaId) filtros.categoria_id = dados.categoriaId;
+        if (dados.statusEstoque) filtros.status_estoque = dados.statusEstoque;
+
+        return filtros;
+    }
+
+    // Se houver nome ou codigo, cria condicao de OR
+    const criterios: FindOptionsWhere<Insumo>[] = [];
+    const baseFiltros: FindOptionsWhere<Insumo> = {};
+
+    if (dados.categoriaId) baseFiltros.categoria_id = dados.categoriaId;
+    if (dados.statusEstoque) baseFiltros.status_estoque = dados.statusEstoque;
 
     if (dados.nome) {
-        filtros.nome = ILike(`%${dados.nome}%`);
+        criterios.push({ ...baseFiltros, nome: ILike(`%${dados.nome}%`)});
     }
 
     if (dados.codigo) {
-        filtros.codigo = ILike(`%${dados.codigo}%`);
+        criterios.push({ ...baseFiltros, codigo: ILike(`%${dados.codigo}%`)});
     }
 
-    if (dados.categoriaId) {
-        filtros.categoria_id = dados.categoriaId;
-    }
-
-    return filtros;
+    return criterios;
 }
